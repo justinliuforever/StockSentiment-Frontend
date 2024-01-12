604454a6e3d95614ac16c4a4eafc8bd1dc54dcd0
Justin Liu <justinliu@ark7.com>
2024-01-12 09:45:58 -0800

    update Ticker

--- src/constants/tickers.js
+++ src/constants/tickers.js
@@ -1,8 +1,8 @@
 // src/constants/tickers.js
 export const tickers = ['U', 'AAPL', 'MSFT', 'AMZN', 'NVDA', 'GOOGL', 'META', 'TSLA', 'GOOG', 
-  'BRK.B', 'AVGO', 'UNH', 'JPM', 'FB', 'PYPL', 'V', 'HD', 'BABA', 'INTC', 'CMCSA', 'ADBE', 'PFE', 
-  'NFLX', 'TSM', 'CRM', 'KO', 'NKE', 'ABNB', 'XOM', 'PEP', 'PPGGST'];
+'BRK.B', 'AVGO', 'UNH', 'JPM', 'PYPL', 'V', 'HD', 'BABA', 'INTC', 'CMCSA', 'ADBE', 'PFE', 
+'NFLX', 'TSM', 'CRM', 'KO', 'NKE', 'ABNB', 'XOM', 'PEP', 'LDOS', 'CPRT', 'GILD', 'LRCX', 'BLDR', 'PPGGST'];
 
   export const display_Tickers = ['U', 'AAPL', 'MSFT', 'AMZN', 'NVDA', 'GOOGL', 'META', 'TSLA', 'GOOG', 
-  'BRK.B', 'AVGO', 'UNH', 'JPM', 'FB', 'PYPL', 'V', 'HD', 'BABA', 'INTC', 'CMCSA', 'ADBE', 'PFE', 
-  'NFLX', 'TSM', 'CRM', 'KO', 'NKE', 'ABNB', 'XOM', 'PEP'];
+  'BRK.B', 'AVGO', 'UNH', 'JPM', 'PYPL', 'V', 'HD', 'BABA', 'INTC', 'CMCSA', 'ADBE', 'PFE', 
+  'NFLX', 'TSM', 'CRM', 'KO', 'NKE', 'ABNB', 'XOM', 'PEP', 'LDOS', 'CPRT', 'GILD', 'LRCX', 'BLDR'];
